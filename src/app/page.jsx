b81d83e0176bdc116e3d7bc70b5b945f4e399c14
Rohit <101079442+Singh8700@users.noreply.github.com>
2025-02-04 23:31:08 +0530

    commit for more 2nd style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,12 +7,21 @@ import Link from 'next/link';
 import Footer from './footer/footer';
 
 export default function Home() {
+  const randNumber = Math.floor(Math.random() * 3);
+  // console.log(randNumber);
   return (
     <>
     <div className="containers">
 
     <main className={style.main}>
     <Background/>
+    <motion.div className={style.overlay} 
+     initial={{ opacity: 0, y: 20 }}
+     animate={{ opacity: 1, y: 0 }}
+     transition={{ duration: 0.8 }}>
+      <span></span>
+      <img src={`${randNumber == 0 ? '/imgs/pic03.png' : '/imgs/pic02.png'}`} alt="itsme" />
+    </motion.div>
       <div className={style.wrapper}>
         
         <motion.div 
